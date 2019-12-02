028432be9f5002dcd2f84c289587c2575aef24eb
Michael Messerli <michaelamesserli@gmail.com>
2019-12-02 17:23:25 +0700

    :rocket: hardcode firebaseConfig

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: String(process.env.FIREBASE_API),
-  authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
-  databaseURL: String(process.env.FIREBASE_DB_URL),
-  projectId: String(process.env.FIREBASE_PROJECT_ID),
-  storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET),
-  messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
-  appId: String(process.env.FIREBASE_APP_ID)
+  apiKey: 'AIzaSyDF3IralvpqS0dqggjyfdPUbRbfJ2bOcQo',
+  authDomain: 'job-hunt-buddy.firebaseapp.com',
+  databaseURL: 'https://job-hunt-buddy.firebaseio.com',
+  projectId: 'job-hunt-buddy',
+  storageBucket: 'job-hunt-buddy.appspot.com',
+  messagingSenderId: '455609997246',
+  appId: '1:455609997246:web:dbc193c4ab01efea670bef'
 }
 
 export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
