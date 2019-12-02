85d74f79182850abfc062abcffc6441c51b5f015
Michael Messerli <michaelamesserli@gmail.com>
2019-12-02 17:15:55 +0700

    :rocket: force all firebaseConfig values to be String

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
+  apiKey: String(process.env.FIREBASE_API),
+  authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
+  databaseURL: String(process.env.FIREBASE_DB_URL),
   projectId: String(process.env.FIREBASE_PROJECT_ID),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(process.env.FIREBASE_APP_ID)
 }
 
 export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
