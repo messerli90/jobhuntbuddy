605e17132ee904358bb656f35b77958144ac81d6
Michael Messerli <michaelamesserli@gmail.com>
2019-12-04 00:40:56 +0700

    :lipstick: Update Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || 'JobHuntBuddy',
+    title: 'JobHuntBuddy | It\'s how you find the job.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
